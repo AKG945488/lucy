0b054aa63b93fc2592390ef7fd9f2e8ba0840645
AKG945488 <105988316+AKG945488@users.noreply.github.com>
2022-06-04 16:08:56 +0530

    Add files via upload

---
+++ LUCYs/modules/shout.py
@@ -0,0 +1,17 @@
+from LUCY import dispatcher
+from LUCY import Update
+from LUCY.ext import CallbackContext
+from LUCY.modules.helper_funcs.decorators import lucy
+
+@lucy(command='shout')
+def shout(update: Update, context: CallbackContext):
+    args = context.args
+    text = " ".join(args)
+    result = [" ".join(list(text))]
+    for pos, symbol in enumerate(text[1:]):
+        result.append(symbol + " " + "  " * pos + symbol)
+    result = list("\n".join(result))
+    result[0] = text[0]
+    result = "".join(result)
+    msg = "```\n" + result + "```"
+    return update.effective_message.reply_text(msg, parse_mode="MARKDOWN")
